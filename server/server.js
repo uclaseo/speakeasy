fdf924f429b19c47a1635cf3a994f47fc1401952
Joshua Bahr <joshuabahr73@gmail.com>
2017-07-08 17:03:11 -0700

    socketEvents

--- server/server.js
+++ server/server.js
@@ -21,23 +21,16 @@ app.use(morgan('dev'));
 
 app.use('/', router);
 
-<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, '../public')));
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-})
-=======
-app.use(express.static(path.join(__dirname, '../')));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
->>>>>>> add socket.io server
 
 init()
   .then(() => {
