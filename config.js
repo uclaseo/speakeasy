660b9cad1a2e208519efb760e9f9537a36885881
Joshua Bahr <joshuabahr73@gmail.com>
2017-07-13 15:27:11 -0700

    delete config

--- config.js
+++
@@ -1,9 +0,0 @@
-let nateElephantSQL =
-  'postgres://gkslfboe:KtRSQK-dJ2gsk7ki5tGhJNPHDeRgqBMs@stampy.db.elephantsql.com:5432/gkslfboe';
-const dbUrl = 'postgres://oxnpckfb:zXf49-tRBzbO2vd7HqXgvNUh-ExBMlxE@pellefant.db.elephantsql.com:5432/oxnpckfb';
-
-let inseokElephantSQL =
-  'postgres://yvkyoifu:Zz2k5is9ojQvyf1OgEduwdHdUjAmCHLm@pellefant.db.elephantsql.com:5432/yvkyoifu';
-
-
-module.exports = dbUrl;
