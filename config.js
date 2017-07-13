75932179564f497e43ad54bb1c93ed28264ba308
Inseok Seo <inseok.ucla@gmail.com>
2017-07-13 13:27:20 -0700

    dont touch my database

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let nateElephantSQL =
   'postgres://gkslfboe:KtRSQK-dJ2gsk7ki5tGhJNPHDeRgqBMs@stampy.db.elephantsql.com:5432/gkslfboe';
 
 let inseokElephantSQL =
-  'postgres://yvkyoifu:Zz2k5is9ojQvyf1OgEduwdHdUjAmCHLm@pellefant.db.elephantsql.com:5432/yvkyoifu';
+  'postgres://yhylhcnk:XFzbBAYwJq01G2JMFS6cUJrPCWATjOgC@stampy.db.elephantsql.com:5432/yhylhcnk';
 
 let dbUrl = inseokElephantSQL;
 
