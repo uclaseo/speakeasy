b6e32fedd263ec4ec74cffd609c0fb750d4e30c8
Inseok Seo <inseok.ucla@gmail.com>
2017-07-13 15:13:05 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-let nateElephantSQL =
-  'postgres://gkslfboe:KtRSQK-dJ2gsk7ki5tGhJNPHDeRgqBMs@stampy.db.elephantsql.com:5432/gkslfboe';
-
-let inseokElephantSQL = 'postgres://tobrzbfh:_c-_GHPCvqi3tB1r1-61pbSHmOE98Mz8@babar.elephantsql.com:5432/tobrzbfh';
-
-let dbUrl = inseokElephantSQL;
-
-module.exports = dbUrl;
