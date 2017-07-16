015ff7aec23e1fd31ccc74350e805f0454118fe8
Michael Cheng <michaelcheng1991@gmail.com>
2017-07-15 17:43:24 -0700

    added nearbyEvents with userLocation

--- public/src/containers/home.js
+++ public/src/containers/home.js
@@ -6,7 +6,8 @@ import Auth from '../Auth0/Auth0';
 import {bindActionCreators} from 'redux';
 import {fetchProfile} from '../actions/authAction';
 import SimpleForm from './event_setting';
-
+// import turf from 'turf-distance'
+import turf from 'turf'
 const ROOT_URL = 'localhost:8080';
 
 const auth = new Auth();
@@ -15,16 +16,91 @@ class Home extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      userLocation : [],
+      nearByEvents : []
+    }
+
     this.registerUser = this.registerUser.bind(this);
     this.getNearbyEvents = this.getNearbyEvents.bind(this);
+    this.getUserLocation = this.getUserLocation.bind(this);
   }
 
   componentDidMount() {
     auth.getProfile((error, profile) => {
       this.registerUser(profile);
     });
+    this.getUserLocation(this.getNearbyEvents);
+    // this.getNearbyEvents();
+  }
 
 
+
+  
+
+  getUserLocation(cb){
+    console.log("are we in getUserLocation?")
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+          // console.log("getting position via html5", position.coords)
+          this.setState({
+            userLocation : [position.coords.latitude, position.coords.longitude]
+          }, () => {
+            console.log("what is the user location?", this.state.userLocation);
+            cb();
+          })
+          
+        });
+    } else { 
+        console.log("Geolocation is not supported by this browser.");
+    }
+    
+  }
+
+
+  getNearbyEvents(){
+    axios.get("/api/event/searchevents")
+    .then((response)=>{
+      console.log("before we compare, this.state.userLocation is", this.state.userLocation)
+      for (var i = 0; i < response.data.length; i ++){
+        if (this.getDistance([this.state.userLocation[0], this.state.userLocation[1]], [response.data[i].latitude, response.data[i].longitude])){
+          this.setState({
+            nearByEvents: [...this.state.nearByEvents, response.data[i]]
+          })
+          console.log("we got a nearByEvent", this.state.nearByEvents);
+        }
+      } 
+    })
+    .catch((error) =>{
+      console.log("getNearbyEvents get request failed", error)
+    })
+  }
+
+  getDistance(fromPoint, toPoint){
+    var from = {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [fromPoint[0], fromPoint[1]]
+      }
+    };
+    var to = {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [toPoint[0], toPoint[1]]
+      }
+    };
+    var points = {
+      "type": "FeatureCollection",
+      "features": [from, to]
+    };
+    var distance = turf.distance(from, to, "miles");
+    console.log("distance between two points", distance);
+    return distance < 0.5;
   }
 
   registerUser(profile) {
@@ -37,18 +113,6 @@ class Home extends Component {
       console.log('this is error', error);
     })
   }
-
-
-  getNearbyEvents(){
-    axios.get("/api/event/searchevents")
-    .then((response)=>{
-      console.log("getNearbyEvents",response)
-    })
-    .catch((error) =>{
-      console.log("getNearbyEvents get request failed", error)
-    })
-  }
-
 
   render() {
     
@@ -72,7 +136,6 @@ class Home extends Component {
               />
             </div>
           </div>
-          {this.getNearbyEvents()}  
           <br />
           <br />
           <Link to="/event_setting">
@@ -80,7 +143,15 @@ class Home extends Component {
               type="button" className="btn btn-secondary btn-lg myBtns">Create Event
             </button>
           </Link>
-        
+          {this.state.nearByEvents.map((event)=>{
+            return <div> {event.eventName} </div>
+          })}
+      
+          {/* {this.getNearbyEvents() ? this.getNearbyEvents().map((event)=>{
+              return <li> {event.eventName}</li>
+          }): null}            */}
+          {/* {this.getUserLocation()} */}
+          {/* {console.log(this.getDistance([0,0],[0,0]))} */}
         </div>
       </div>
     );
