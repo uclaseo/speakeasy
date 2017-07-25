75ae2d58eae5bb2eab9fa5d4b0f832d550aa91de
Inseok Seo <inseok.ucla@gmail.com>
2017-07-25 16:40:06 -0700

    produce dummy data' ?

--- server/dummy.js
+++ server/dummy.js
@@ -30,10 +30,257 @@ const userDummy = [
     photo: 'http://bit.ly/2ugy2qm',
     latitude: 51.5033640,
     longitude: -0.1276250
+  },
+  {
+    name: 'Jason Chao',
+    handle: 'Ping Pong Master',
+    email: 'jason.chao@hackreactor.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'William',
+    handle: 'Will I am',
+    email: 'william@hackreactor.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'John',
+    handle: 'Ripped as Hell',
+    email: 'buffdude@hackreactor.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'Adewale',
+    handle: 'Big Dude',
+    email: 'bigdude@hackreactor.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'Kay',
+    handle: 'okay',
+    email: 'kay@hackreactor.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'Howard',
+    handle: 'Hwat?',
+    email: 'WhatHwat@gmail.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'Max',
+    handle: 'Wolfgang',
+    email: 'wolfgang@gmail.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'Sho',
+    handle: 'shonozaki',
+    email: 'shonozaki@gmail.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'Jay',
+    handle: 'um',
+    email: 'jay@gmail.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
+  },
+  {
+    name: 'Lewis',
+    handle: 'Lewgular',
+    email: 'angular@gmail.com',
+    photo: 'http://bit.ly/2ugy2qm',
+    latitude: 51.5033640,
+    longitude: -0.1276250
   }
 ];
 
+const eventDummy = [
+  {
+    eventName: 'Inseok\'s Crazy Party',
+    password: '123',
+    latitude: 33.9759614,
+    longitude: -118.3911292,
+    isLive: true,
+    eventPhoto: 'http://az616578.vo.msecnd.net/files/2017/02/28/6362384699061998551682913604_HERO_IBIZA_CLOSING_PARTIES_Privilege.jpg',
+    description: 'The Coolest Party',
+    userId: 2
+  },
+  {
+    eventName: 'Reptile lovers',
+    password: '123',
+    latitude: 33.9759614,
+    longitude: -118.3911292,
+    isLive: true,
+    eventPhoto: 'https://phylumsofanimals.files.wordpress.com/2013/01/291439-alexfas01.jpg',
+    description: 'Do you like Reptiles?',
+    userId: 4
+  },
+  {
+    eventName: 'HRLA Graduation',
+    password: '123',
+    latitude: 33.9759614,
+    longitude: -118.3911292,
+    isLive: true,
+    eventPhoto: 'https://static1.squarespace.com/static/ta/522a22cbe4b04681b0bff826/3003/assets/images/logo/hack-reactor-logo.png',
+    description: 'The End of Hell',
+    userId: 1
+  },
+  {
+    eventName: 'Programming with Lewis',
+    password: '123',
+    latitude: 33.9759614,
+    longitude: -118.3911292,
+    isLive: true,
+    eventPhoto: 'https://readwrite.com/wp-content/uploads/programming-languages-for-iot-e1467856370607.jpg',
+    description: 'All In Angular',
+    userId: 14
+  },
+  {
+    eventName: 'Ping Pong Party',
+    password: '123',
+    latitude: 33.9759614,
+    longitude: -118.3911292,
+    isLive: true,
+    eventPhoto: 'http://img.wennermedia.com/article-leads-horizontal/rs-atlanta-falcons-06ee1175-4e0d-4cbc-a6f5-68326b71ea67.jpg',
+    description: 'Jason Ping Pong',
+    userId: 5
+  },
+]
+
+const userEventDummy = [
+  {
+    userId: 2,
+    eventId: 1
+  },
+  {
+    userId: 4,
+    eventId: 2
+  },
+  {
+    userId: 1,
+    eventId: 3
+  },
+  {
+    userId: 14,
+    eventId: 4
+  },
+  {
+    userId: 1,
+    eventId: 1
+  },
+  {
+    userId: 2,
+    eventId: 2
+  },
+  {
+    userId: 2,
+    eventId: 3
+  },
+  {
+    userId: 1,
+    eventId: 2
+  },
+  {
+    userId: 4,
+    eventId: 1
+  },
+  {
+    userId: 5,
+    eventId: 1
+  },
+  {
+    userId: 6,
+    eventId: 1
+  },
+  {
+    userId: 6,
+    eventId: 4
+  },
+  {
+    userId: 7,
+    eventId: 4
+  },
+  {
+    userId: 6,
+    eventId: 5
+  },
+  {
+    userId: 7,
+    eventId: 5
+  },
+  {
+    userId: 6,
+    eventId: 2
+  },
+  {
+    userId: 7,
+    eventId: 2
+  },
+  {
+    userId: 5,
+    eventId: 5
+  },
+  {
+    userId: 4,
+    eventId: 3
+  },
+  {
+    userId: 8,
+    eventId: 1
+  },
+  {
+    userId: 8,
+    eventId: 2
+  },
+  {
+    userId: 8,
+    eventId: 3
+  },
+  {
+    userId: 9,
+    eventId: 2
+  },
+  {
+    userId: 9,
+    eventId: 3
+  },
+  {
+    userId: 9,
+    eventId: 4
+  }
+]
+
+const crossPathDummy = [
+  {id: 1, count: 3, userId: 1, possibleId: 2, chatting: false},
+  {id: 2, count: 2, userId: 3, possibleId: 1, chatting: false},
+  {id: 3, count: 1, userId: 1, possibleId: 4, chatting: true},
+  {id: 4, count: 5, userId: 5, possibleId: 1, chatting: false},
+  {id: 5, count: 4, userId: 1, possibleId: 8, chatting: false},
+  {id: 6, count: 4, userId: 9, possibleId: 1, chatting: false},
+]
 
 module.exports = {
-  userDummy: userDummy
+  userDummy: userDummy,
+  eventDummy: eventDummy,
+  crossPathDummy: crossPathDummy,
+  userEventDummy: userEventDummy
 }
